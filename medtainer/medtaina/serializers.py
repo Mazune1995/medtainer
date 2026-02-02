213d3380a8b07c45335aa01283e1c5315a861ba1
Mazune <1995mariamnasser@gmail.com>
2026-02-02 23:01:25 +0300

    Prepare project for Render deployment

--- medtainer/medtaina/serializers.py
+++ medtainer/medtaina/serializers.py
@@ -1,12 +1,19 @@
-from rest_framework import serializers
+from rest_framework import serializers # type: ignore
+
 from .models import InventoryItem, User
 
+
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
+
         model = User
+
         fields = ['id', 'username', 'email', 'is_staff']
 
         class InventoryItemSerializer(serializers.ModelSerializer):
+
             class Meta:
                 model = InventoryItem
-                fields = ['id', 'name', 'description', 'quantity', 'price', 'last_updated'] 
+
+                fields = '__all__'
--- medtainer/medtaina/views.py
+++ medtainer/medtaina/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets # type: ignore
 
 from .models import InventoryItem, User
 
