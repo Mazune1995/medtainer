c1234685f40dddfa4fbeabe270a6454b09899ac7
Mazune <1995mariamnasser@gmail.com>
2026-02-03 20:09:40 +0300

    Fix INSTALLED_APPS and WSGI for Render

--- medtainer/wsgi.py
+++ medtainer/wsgi.py
@@ -10,6 +10,5 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medtainer.settings')  # if settings.py is in root
 application = get_wsgi_application()
