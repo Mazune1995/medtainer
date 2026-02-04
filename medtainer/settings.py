327a968d14fb99d53d3c4fdc2cc72e908179e4a4
Mazune <1995mariamnasser@gmail.com>
2026-02-04 18:39:53 +0300

    Ready deployment

--- medtainer/settings.py
+++ medtainer/settings.py
@@ -53,19 +53,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'medtainer.wsgi.application'
 
-import dj_database_url
-from pathlib import Path
 import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("RENDER") == "True":
+    # Production on Render
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT"),
+            "OPTIONS": {"sslmode": "require"},
+        }
+    }
+else:
+    # Local dev
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 STATIC_URL = 'static/'
 
